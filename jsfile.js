b267e16ec9dfce81b120df78099e02ca4f0c260d
Meghana R <40672615+meghana117@users.noreply.github.com>
2019-05-19 01:17:36 +0530

    Add files via upload

--- jsfile.js
+++ jsfile.js
@@ -1,28 +1,36 @@
 var unitIsCelcius = true;
 var globalForecast = [];
 
-var data = JSON.parse('{"coord":{"lon":77.59,"lat":12.98},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03d"}],"base":"stations","main":{"temp":304.8,"pressure":1011,"humidity":43,"temp_min":303.71,"temp_max":306.15},"visibility":10000,"wind":{"speed":1,"deg":340},"clouds":{"all":40},"dt":1558179498,"sys":{"type":1,"id":9205,"message":0.0054,"country":"IN","sunrise":1558139032,"sunset":1558184907},"id":1277333,"name":"Bengaluru","cod":200}');
+
+var data = JSON.parse('{"coord":{"lon":77.59,"lat":12.98},"weather":{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03d"},"base":"stations","main":{"temp":304.8,"pressure":1011,"humidity":43,"temp_min":303.71,"temp_max":306.15},"visibility":10000,"wind":{"speed":1,"deg":340},"clouds":{"all":40},"dt":1558179498,"sys":{"type":1,"id":9205,"message":0.0054,"country":"IN","sunrise":1558139032,"sunset":1558184907},"id":1277333,"name":"Bengaluru","cod":200}');
 
 
 
 $(function(){
   getClientPosition();
   startClock();  
-  document.getElementById("city-name").innerHTML=data.name;
-  document.getElementById("city-code").innerHTML=data.sys.country;
+  document.getElementById("cityName").innerHTML=data.name;
+  document.getElementById("cityCode").innerHTML=data.sys.country;
   document.getElementById("humidity").innerHTML=data.main.humidity;
   document.getElementById("precipitation").innerHTML="0";
   document.getElementById("wind").innerHTML=data.wind.speed;
-  var sunrise=new Date(data.sys.sunrise)
-  var sunset=new Date(data.sys.sunset)
+  var sunrise=new Date(data.sys.sunrise).toLocaleTimeString();
+  var sunset=new Date(data.sys.sunset).toLocaleTimeString();
+
   document.getElementById("sunrise").innerHTML=sunrise;
   document.getElementById("sunset").innerHTML=sunset;
   document.getElementById("cloud").innerHTML=data.clouds.all;
   document.getElementById("dew").innerHTML=data.dew;
   document.getElementById("visibility").innerHTML=data.visibility;
-  document.getElementById("localtime").innerHTML=new Date().toLocaleTimeString();
-  document.getElementById("localdate").innerHTML=new Date().toLocaleDateString();
+  document.getElementById("localTime").innerHTML=new Date().toLocaleTimeString();
+  document.getElementById("localDate").innerHTML=new Date().toLocaleDateString();
+  document.getElementById("tempDescription").innerHTML=data.weather.description;
+  //convert from kelvin to celsius
+  var temp=Math.round((data.main.temp-273.15)*100)/100
+  // Math.round(temp)
+  document.getElementById("mainTemperature").innerHTML=temp;
 });
+
 
 
 function startClock(){
@@ -33,44 +41,34 @@ function startClock(){
 
 
 function getClientPosition(){
-
-  $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Bengaluru&appid=3617f87cdfb13c8f1541d6a2c7700a07", function(position) {
-    console.log(position);
+  $.getJSON("https://ipapi.co/json/", function(position) {
     $("#cityName").text(position.city);
     $("#cityCode").text(position.country);
     
     getWeatherData(position.latitude, position.longitude);
-
   });
 }
 
 
 function getWeatherData(latitude, longitude){
   $.ajax({
-    type: "POST",
-    url: "https://api.openweathermap.org/data/2.5/weather?q=Bengaluru&appid=3617f87cdfb13c8f1541d6a2c7700a07" ,
+    type: "GET",
+    url: "https://api.openweathermap.org/data/2.5/weather?q=Bengaluru&appid=3617f87cdfb13c8f1541d6a2c7700a07" + latitude + "&lon=" + longitude + "&units=metric&cnt=5",
     cache: true,
     headers: {
       "Access-Control-Allow-Headers": "x-requested-with"
     },
     success: function(forecast){
-      console.log(forecast);
       globalForecast = forecast;
       updateForecast(forecast);
 
     },
-    error: function(error){
-      console.log("Error with ajax: "+ error);
-    }
+ 
   });
 }
 
 
 function updateForecast(forecast){
-
-
-  document.getElementById("city-name").innerHTML="BANGALORE";
-
 
   var today = forecast.list[0];
   $("#tempDescription").text(toCamelCase(today.weather[0].description));
@@ -86,7 +84,7 @@ function updateForecast(forecast){
   for(var i = 1; i < (forecast.list).length; i++){
     var day = forecast.list[i];
 
-    
+    var dayName = getFormattedDate(day.dt).substring(0,3);
 
     var weatherIcon = weatherIconsMap[day.weather[0].icon];
 
@@ -99,7 +97,12 @@ function updateForecast(forecast){
 }
 
 
-// Celcius button handler.
+$("#refreshButton").on("click", function(){
+  // Starts Refresh button's spinning animation
+  $("#refreshButton").html("<i class='fa fa-refresh fa-spin fa-fw'></i>");
+  getWeatherData();
+});
+
 
 $("#celcius").on("click", function(){
   if(!unitIsCelcius){
@@ -113,7 +116,7 @@ $("#celcius").on("click", function(){
     globalForecast.list[0] = today;
 
     // week
-    for(var i = 1; i < 5; i ++){
+    for(var i = 1; i < 7; i ++){
       var weekDay = globalForecast.list[i];
       weekDay.temp.day = toCelcius(weekDay.temp.day);
       weekDay.temp.max = toCelcius(weekDay.temp.max);
@@ -121,14 +124,12 @@ $("#celcius").on("click", function(){
       globalForecast[i] = weekDay;
     }
 
+
     updateForecast(globalForecast);
 
     unitIsCelcius = true;
   }
 });
-
-
-// Farenheit button handler
 
 $("#farenheit").on("click", function(){  
   if(unitIsCelcius){
@@ -141,7 +142,8 @@ $("#farenheit").on("click", function(){
     today.temp.min = toFerenheit(today.temp.min);
     globalForecast.list[0] = today;
 
-    for(var i = 1; i < 5; i ++){
+    // week
+    for(var i = 1; i < 7; i ++){
       var weekDay = globalForecast.list[i];
       weekDay.temp.day = toFerenheit(weekDay.temp.day);
       weekDay.temp.max = toFerenheit(weekDay.temp.max);
@@ -172,11 +174,13 @@ function toCamelCase(str) {
 }
 
 
+// Converts to Celcius
 function toCelcius(val){
   return Math.round((val - 32) * (5/9));
 }
 
 
+// Converts to Farenheit
 function toFerenheit(val){
   var degrees = (val * 1.8) + 32;
   var rounded = Math.round(degrees);
